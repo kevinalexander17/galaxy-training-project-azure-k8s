ec128a3839a5b329a6527b0b45fc046a2dea0034
kevinalexander17 <qalexander370@gmail.com>
2026-03-29 13:05:57 -0500

    feat: add centralized message constants
    - Add USER_REGISTERED_SUCCESSFULLY constant
    - Add USER_LOGGED_IN_SUCCESSFULLY constant
    - Rename ROLE_CLAIM to CLAIM_ROLE for consistency
    - Add CLAIM_USER_NAME constant for JWT token structure
    
    Improves maintainability by centralizing response messages
    and standardizing JWT claim naming convention

--- ms-authentication/src/main/java/com/kodenca/ms_authentication/util/Constants.java
+++ ms-authentication/src/main/java/com/kodenca/ms_authentication/util/Constants.java
@@ -9,7 +9,10 @@ public class Constants {
     public static final String USER_ALREADY_EXISTS = "User already exists: ";
     public static final String INVALID_PASSWORD = "Invalid password";
     public static final String AUTHORIZATION = "Authorization";
-    public static final String ROLE_CLAIM = "role";
+    public static final String CLAIM_ROLE = "role";
     public static final String BEARER = "Bearer ";
+    public static final String CLAIM_USER_NAME = "userName";
+    public static final String USER_REGISTERED_SUCCESSFULLY = "User registered successfully";
+    public static final String USER_LOGGED_IN_SUCCESSFULLY = "User logged in successfully";
 
 }
